a992e75a78043d8b0eef88ca92953e6f478248d3
Victor Allen <vamuigua@gmail.com>
2019-06-11 11:53:11 +0300

    Add frontend static page for freecodecamp

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fnbo5-1.fna.fbcdn.net/vp/2b516720aa07096164d4b4495fa58f6c/5D8CE438/t51.2885-19/s150x150/22709172_932712323559405_7810049005848625152_n.jpg?_nc_ht=instagram.fnbo5-1.fna.fbcdn.net" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fnbo5-1.fna.fbcdn.net/vp/b514b1c9070bd76c8eb03e9de668df50/5D8B454B/t51.2885-15/sh0.08/e35/c0.74.877.877a/s640x640/61129659_484895502256050_2457635576497186056_n.jpg?_nc_ht=instagram.fnbo5-1.fna.fbcdn.net" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fnbo5-1.fna.fbcdn.net/vp/33227c6cbb95510dd5614b542e821832/5D991213/t51.2885-15/sh0.08/e35/c0.0.749.749a/s640x640/62405980_352431338752162_8074657814463950764_n.jpg?_nc_ht=instagram.fnbo5-1.fna.fbcdn.net" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fnbo5-1.fna.fbcdn.net/vp/ea9ca0ae93fb52216b9a7c9e3d764afb/5D9C4049/t51.2885-15/sh0.08/e35/c0.78.883.883a/s640x640/60721114_308501250092136_1832984087716609793_n.jpg?_nc_ht=instagram.fnbo5-1.fna.fbcdn.net" class="w-100">
         </div>
     </div>
 </div>
