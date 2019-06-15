2bc0f6bf6bd77b5c6fa590427682e8beb27303f2
Victor Allen <vamuigua@gmail.com>
2019-06-15 13:43:25 +0300

    Modify saving of user's post image and redirecting to user's profile

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,15 +6,35 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+
+    //validate user for login
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    //create page for posts
     public function create(){
     	return view('posts.create');
     }
 
+    //save post
     public function store(){
+        //validate post data from user
     	$data = request()->validate([
     		'caption' => 'required',
-    		'image' => 'required',
+    		'image' => ['required', 'image'],
     	]);
-    	dd(request()->all());
+
+        //save instance image path
+        $imagePath = request('image')->store('uploads', 'public');
+
+        //authenticate user before creating posts and save the post
+        auth()->user()->posts()->create([
+            'caption' => $data['caption'],
+            'image' => $imagePath,
+        ]);
+
+        //redirect to authenticated user's profile
+    	return redirect('/profile/' . auth()->user()->id);
     }
 }
