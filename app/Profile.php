687905e165acc370ee9f26eca97d19a4e02675a8
Victor Allen <vamuigua@gmail.com>
2019-06-26 13:09:48 +0300

    Add default-profile-image for user after registration

--- app/Profile.php
+++ app/Profile.php
@@ -9,6 +9,11 @@ class Profile extends Model
 
 	protected $guarded = [];
 
+	public function profileImage(){
+		$imagePath = ($this->image) ? $this->image : 'profile/DtzevtC07rqgPhT3rjX7eVGFu3TK1R1YH5c1cz1G.png';
+		return '/storage/' . $imagePath;
+	}
+
     public function user(){
     	return $this->belongsTo(User::class);
     }
