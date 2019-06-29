50aada1433673adc339a028977089ba999b9b762
Victor Allen <vamuigua@gmail.com>
2019-06-29 18:59:34 +0300

    Add Mail to send Welcome Message upon Registering

--- app/User.php
+++ app/User.php
@@ -2,9 +2,11 @@
 
 namespace App;
 
+use App\Mail\NewUserWelcomeMail;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\Mail;
 
 class User extends Authenticatable
 {
@@ -44,6 +46,9 @@ class User extends Authenticatable
             $user->profile()->create([
                 'title' => $user->username
             ]);
+
+            Mail::to($user->email)->send(new NewUserWelcomeMail());
+
         });
     }
 
